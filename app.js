5cd36af9a7a059ae43304a0f5a02c0c1a94f25fc
shafik720 <shafikrasel5@gmail.com>
2022-09-30 22:19:16 +0600

    dsfgsdfg

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 
 const addBox = document.querySelector('.add-box'),
-popupBox = document.querySelector('.popup-box')
+popupBox = document.querySelector('.popup-box'),
+closeIcon = popupBox.querySelector(".popup-header span i")
 
 addBox.addEventListener('click',()=>{
     popupBox.classList.add('show');
+})
+
+closeIcon.addEventListener('click',()=>{
+    popupBox.classList.remove('show');
 })
